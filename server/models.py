c384331179fb27335bea4270f203c6eff6a7376a
Samantha Genevay <samanthagenevay@gmail.com>
2023-04-17 09:02:24 -0700

    Completed model setup

--- server/models.py
+++ server/models.py
@@ -1,5 +1,49 @@
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+class Order(db.Model, SerializerMixin):
+    __tablename__ = 'orders'
+
+    serialize_rules = ('-dish_orders', )
+
+    id = db.Column(db.Integer, primary_key=True)
+    customer_name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    dish_orders = db.relationship('DishOrder', backref='order')
+    potato_dishes = association_proxy('dish_orders', 'potato_dish')
+
+
+class PotatoDish(db.Model, SerializerMixin):
+    __tablename__ = 'potato_dishes'
+
+    serialize_rules = ('-dish_orders', )
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    base = db.Column(db.String)
+    sauce = db.Column(db.String)
+    topping = db.Column(db.String)
+    price = db.Column(db.Float)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    dish_orders = db.relationship('DishOrder', backref='potato_dish')
+    orders = association_proxy('dish_orders', 'order')
+
+
+class DishOrder(db.Model, SerializerMixin):
+    __tablename__ = 'dish_orders'
+
+    serialize_rules = ('-order', '-potato_dish')
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'))
+    potato_dish_id = db.Column(db.Integer, db.ForeignKey('potato_dishes.id'))

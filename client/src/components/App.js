8176f15a3874b8749487abf1cb680e9c5153d6f1
Grace Nieboer <grace.nieboer@outlook.com>
2023-04-17 15:27:02 -0400

    client and server connected

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,16 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  // Code goes here!
+ 
+
+
+  return (
+    <div>
+      Peace and Love
+    </div>
+
+  )
+
 }
 
 export default App;

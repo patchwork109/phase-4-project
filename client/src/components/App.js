642fe4bf16d0c3ad0c93c72b838311ba85cd8a2a
Grace Nieboer <grace.nieboer@outlook.com>
2023-04-19 16:52:09 -0400

    footer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -39,6 +39,7 @@ function App() {
           </Route>
         </Switch>
       </Router>
+      <Footer />
     </div>
 
   )
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,14 +1,39 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
+
 
 function Footer() {
  
-
-
   return (
-    <div>
-      I'm the footer!
+    <footer className="footer">
+    <div className="footer-left col-md-4 col-sm-6">
+      <p className="about">
+        <span> About the company</span> We believe that French fries are more than just a snack – they are a culinary experience. We are committed to providing our customers with the best possible experience, from the moment they place their order to the moment they take their first bite. Come experience fries like never before.
+      </p>
+      
     </div>
-
+    <div className="footer-center col-md-4 col-sm-6">
+      <div>
+        <i className="fa fa-map-marker"></i>
+        <p><span> 101 BestSpuds Drive</span> Idaho, United States</p>
+      </div>
+      <div>
+        <i className="fa fa-phone"></i>
+        <p> (1) 800 SPUDLUV </p>
+      </div>
+      <div>
+        <i className="fa fa-envelope"></i>
+        <p><a href="#"> getfried@getfried.com</a></p>
+      </div>
+    </div>
+    <div className="footer-right col-md-4 col-sm-6">
+      <h2> Company<span> logo</span></h2>
+      <p className="menu">
+        This app was made with coffee, love, and preworkout ♡
+      </p>
+      <p className="name"> Get FRIED &copy; 2023</p>
+    </div>
+  </footer>
   )
 
 }

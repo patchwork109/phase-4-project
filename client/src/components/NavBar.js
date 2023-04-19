c611042f2a650e53737f7eb841d0d243c7e53398
Samantha Genevay <samanthagenevay@gmail.com>
2023-04-19 12:37:09 -0700

    Added navbar styling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,21 +1,27 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
- 
-const NavBar =() => {
+function NavBar() {
 
   return (
-    <nav>
-      <NavLink exact to="/">Home</NavLink>
-      <NavLink exact to="/menu">Menu</NavLink>
-      <NavLink exact to="/order">Order</NavLink>
-      <NavLink exact to="/about">About</NavLink>
-      <NavLink exact to="/order/cart">Cart</NavLink>
-    </nav>
-
-  )
-
+    <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand exact to="/" as={NavLink}>FRIES</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link exact to="/" as={NavLink}>Home</Nav.Link>
+            <Nav.Link exact to="/menu" as={NavLink}>Menu</Nav.Link>
+            <Nav.Link exact to="/order" as={NavLink}>Order</Nav.Link>
+            <Nav.Link exact to="/about" as={NavLink}>About</Nav.Link>
+          </Nav>
+          <Nav className="justify-content-end">
+            <Nav.Link exact to="/order/cart" as={NavLink}>Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+  );
 }
-
 
 export default NavBar;
